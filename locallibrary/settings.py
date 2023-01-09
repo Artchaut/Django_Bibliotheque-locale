b1ee90bd2682125a482855d2d37c988db369b284
Artchaut <artichaut432@gmail.com>
2023-01-09 21:43:07 +0100

    book/author-listFinal-commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = "/home/Artichaut/Django_Bibliotheque-locale/catalog/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
