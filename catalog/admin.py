59876ae21d377e27ffee76faab2340d71e0ba1b0
Artchaut <artichaut432@gmail.com>
2023-01-10 21:12:57 +0100

    fix-adminphoto-commit

--- catalog/admin.py
+++ catalog/admin.py
@@ -9,15 +9,15 @@ from catalog.models import Author, Genre, Book, BookInstance
 admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
-
-# Define the admin class
+# Register the admin class with the associated model
+#admin.site.register(Author, AuthorAdmin)
+# Or
+# Register the Admin class using the decorator
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-   fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+   fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death'), 'img_preview']
    readonly_fields = ['img_preview']
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
-
 
 
 # Register the Admin classes for Book using the decorator
